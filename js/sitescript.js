1bf78fcedbdc71b9dd8b8016d5eba31dded6d70e
daviddh231981 <101032678+daviddh231981@users.noreply.github.com>
2022-08-08 20:38:39 -0500

    Added Initial Files

---
+++ js/sitescript.js
@@ -0,0 +1,42 @@
+//JS file created on August 7, 2022, by David Ding
+
+//NAV variables
+const navShowContents = document.querySelector("nav");
+const btnShowNavigation = document.getElementById("navBtn");
+
+//Scroll to Top
+//Get the button:
+scrollBtn = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button and hide navigation.
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+        navShowContents.style.display = "block";
+        btnShowNavigation.innerHTML = "Hide<br>Menu"; 
+    }   
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+//Dropdown menu
+//when button is selected, show navigation.
+function showNav() {
+
+    if (navShowContents.style.display === "none") {
+        navShowContents.style.display = "block";
+        btnShowNavigation.innerHTML = "Hide<br>Menu"; 
+        topFunction();      
+    } else {
+        navShowContents.style.display = "none"
+        btnShowNavigation.innerHTML = "Show<br>Menu";
+    }
+}
